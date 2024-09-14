4a2a728ca4ace6d4d5cce40491589f6843edfd86
Hauke D <haukex@zero-g.net>
2024-09-14 09:05:53 +0000

    Fix devcontainer init

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
+sudo apt update
 sudo apt install -y vim perl-doc cpanminus liblocal-lib-perl
 curl -L https://install.perlbrew.pl | bash
 grep -q -- '-Mlocal::lib' ~/.bashrc || echo "eval \"\$(perl -I\$HOME/perl5/lib/perl5 -Mlocal::lib)\"" >>~/.bashrc
